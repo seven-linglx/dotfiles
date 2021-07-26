88c1db087bd129c5f7f2da95f0df10c8325a7993
linglx <linglx@foxmail.com>
2021-07-26 10:56:34 +0800

    add projectile in config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -48,3 +48,16 @@
   :load-path "modules/private/proto")
 ;; (add-load-path! "modules/private/proto")
 (add-to-list 'auto-mode-alist '("\\.proto\\'" . protobuf-mode))
+
+;; Projectile
+(setq projectile-require-project-root t)
+(use-package projectile
+  :config
+  (cond
+   ;; If fd exists, use it for git and generic projects. fd is a rust program
+   ;; that is significantly faster than git ls-files or find, and it respects
+   ;; .gitignore. This is recommended in the projectile docs.
+   ((executable-find "fd")
+    (setq-default projectile-generic-command "fd . -0 --type f --color=never"))
+   ((executable-find "rg")
+    (setq projectile-generic-command "rg -0 --files --follow --color=never --hidden"))))
